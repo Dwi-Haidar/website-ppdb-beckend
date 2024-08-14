5713b0b2a56a8204d32c7eb94724a243c6b2d860
adiw <adindo48@gmail.com>
2024-08-14 18:35:33 +0700

    add:mdtrans

--- server.ts
+++ server.ts
@@ -1,11 +1,11 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import db from "../beckend-ppdb/src/db/index";
 import router from "./src/route";
 import path from "path";
+import db from "./src/db";
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 app.use(express.json());
--- src/services/ppdbService.ts
+++ src/services/ppdbService.ts
@@ -6,18 +6,63 @@ export const create = async (
   files: { [fieldname: string]: Express.Multer.File[] }
 ) => {
   try {
+    console.log("payload", payload);
+
     const ppdb = await db.ppdb.create({
       data: {
         ...payload,
-        image: {
-          create: files.image.map((img) => ({
-            url: img.filename,
-          })),
-        },
+        // image: {
+        //   create: files.image.map((img) => ({
+        //     url: img.filename,
+        //   })),
+        // },
+      },
+    });
+    console.log("ppob", ppdb);
+
+    // create transaction
+    const midtransClient = require("midtrans-client");
+    let snap = new midtransClient.Snap({
+      isProduction: false,
+      serverKey: "SB-Mid-server-D7115u3C9p40iVIEBH0Xx7-P",
+    });
+
+    let parameter = {
+      transaction_details: {
+        order_id: "ORDER" + ppdb.id,
+        gross_amount: 10000,
+      },
+      credit_card: {
+        secure: true,
+      },
+      callbacks: {
+        finish: "http://localhost:5173/",
+      },
+      customer_details: {
+        name: ppdb.nama,
+        email: ppdb.email,
+        phone: ppdb.noTelp,
+      },
+    };
+
+    const transaction = await snap.createTransaction(parameter);
+    const transactionToken = transaction.token;
+    console.log("transactionToken:", transactionToken);
+
+    const order = await db.order.create({
+      data: {
+        orderId: parameter.transaction_details.order_id,
+        price: parameter.transaction_details.gross_amount,
+        paymentMethod: "",
+        ppdbId: ppdb.id,
       },
     });
 
-    return ppdb;
+    return {
+      ppdb,
+      order,
+      transactionToken,
+    };
   } catch (error) {
     console.error("Error creating PPDB:", error);
     throw error;
@@ -57,6 +102,6 @@ export const deletePpdb = async (id: number) => {
     return ppdb;
   } catch (error) {
     console.error("Error deleting PPDB:", error);
-    throw error; 
+    throw error;
   }
 };

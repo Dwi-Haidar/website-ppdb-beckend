48519e0b243641b3a3977bfabd5df4a692ecebcc
adiw <adindo48@gmail.com>
2024-08-18 14:57:37 +0700

    add:nodemailer

--- server.ts
+++ server.ts
@@ -6,7 +6,7 @@ import path from "path";
 import db from "./src/db";
 import bodyParser from "body-parser";
 dotenv.config();
-const PORT = process.env.PORT || 5001;
+const PORT = `${process.env.PORT}`;
 
 const app = express();
 app.use(express.json());
